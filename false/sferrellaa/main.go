7545c9d0160c109c6a9970ee8c5ad1aff9f4c782
SferrellaA <alexander@sferrella.com>
2019-03-29 23:38:38 +0000

    lol true and false are real unix commands

---
+++ false/sferrellaa/main.go
@@ -0,0 +1,13 @@
+// "false" implementation by SferrellaA
+// File created 29 March, 2019 by SferrellaA
+// File edited 29 March, 2019 by SferrellaA
+
+package main
+
+import "os"
+
+func main() {
+
+	// Return 1, for generic error
+	os.Exit(1)
+}
